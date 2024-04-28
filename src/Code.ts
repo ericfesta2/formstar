ec2d23b54c75905c5618ccae0e9bc4a52876ea1e
Eric Festa <eric.festa2@gmail.com>
2024-04-28 17:31:10 -0400

    Convert configuration properties from top-level variables/setters to passed-in object; optimize writing field names to sheet

--- src/Code.ts
+++ src/Code.ts
@@ -1,8 +1,10 @@
-type ActionRequest = {
-    postData: {
-        contents: string,
-        type: 'recaptcha_v2' | 'recaptcha_v3'
-    }
+type Properties = {
+    sheetName: string,
+    emailSubject: string,
+    formHeading: string,
+    email: string,
+    fields: string[],
+    captcha: CaptchaInfo
 };
 
 type CaptchaInfo = {
@@ -12,82 +14,50 @@ type CaptchaInfo = {
     }
 };
 
-let sheetName = '';
-let emailSubject = 'New submission using FormEasy';
-let formHeading = 'Form submission - FormEasy';
-let email = '';
-let fields: string[] = [];
-let captcha: CaptchaInfo = { type: 'none', data: { secretKey: '' } };
+const activeSpreadsheet = SpreadsheetApp.getActiveSpreadsheet();
 
 const createJsonResponse = (content: object): GoogleAppsScript.Content.TextOutput =>
     ContentService.createTextOutput(JSON.stringify(content)).setMimeType(ContentService.MimeType.JSON);
 
-function setSheet(name: string) {
-    sheetName = name;
-}
+const capitalizeFirstLetter = (str: string): string => str[0].toUpperCase() + str.slice(1);
 
-function setEmail(id: string) {
-    email = id;
-}
-
-function setSubject(subject: string) {
-    emailSubject = subject;
-}
-
-function setFormHeading(heading: string) {
-    formHeading = heading;
-}
-
-function setFields(...fieldsArr: string[]) {
-    fields = [...fieldsArr];
-
-    const length = fields.length;
-    const sheetNameEmpty = sheetName === '';
-
-    const sheet = sheetNameEmpty ?
-        SpreadsheetApp.getActiveSpreadsheet().getActiveSheet() :
-        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
-
-    if (sheet === null) {
-        return createJsonResponse({
-            status: 'error',
-            message: sheetNameEmpty ? 'No spreadsheet found.' : `Spreadsheet ${sheetName} not found.`
-        });
-    }
-
+function setFields(sheet: GoogleAppsScript.Spreadsheet.Sheet, fields: string[]) {
     const firstRow = sheet.getDataRange().getValues()[0];
 
     // Checking if columns in sheet and fields are matching
     if (firstRow.toString() !== '') {
         if (
             firstRow[0].toLowerCase() === fields[0].toLowerCase() &&
-            firstRow[firstRow.length - 2].toString().toLowerCase() === fields[length - 1].toString().toLowerCase()
+            firstRow[firstRow.length - 2].toString().toLowerCase() ===
+                fields[fields.length - 1].toString().toLowerCase()
         ) {
             return;
         }
 
-        if (firstRow.length > length + 1) {
+        if (firstRow.length > fields.length + 1) {
             sheet.getRange(1, 1, 1, 30).clearContent(); // Clearing up to 30 columns
         }
     }
 
-    const formatFirstLetter = (str: string): string => str[0].toUpperCase() + str.slice(1);
-
-    for (let idx = 0; idx < length; idx++) {
-        sheet.getRange(1, idx + 1).setValue(formatFirstLetter(fields[idx]));
-        if (idx === length - 1) {
-            sheet.getRange(1, idx + 2).setValue('Date');
-        }
-    }
+    sheet
+        .getRange(1, 1, 1, fields.length + 1)
+        .setValues([
+            [...fields.map(field => capitalizeFirstLetter(field)), 'Date']
+        ]);
 }
 
-function setRecaptcha(secretKey: string) {
-    captcha = { type: 'recaptcha_v2', data: { secretKey } };
-}
-
-function action(req: ActionRequest): object {
+function action(
+    req: GoogleAppsScript.Events.DoPost, {
+        sheetName = '',
+        emailSubject = 'New Form Submission',
+        formHeading = 'Form Submission',
+        email = '',
+        fields = ['name', 'email', 'message'],
+        captcha = { type: 'none', data: { secretKey: '' } }
+    }: Properties
+): object {
     let { postData: { contents } } = req;
-    let jsonData;
+    let jsonData: { [key: string]: any };
 
     try {
         jsonData = JSON.parse(contents);
@@ -130,42 +100,18 @@ function action(req: ActionRequest): object {
         }
     }
 
-    let logSheet;
-
-    const activeSpreadsheet = SpreadsheetApp.getActiveSpreadsheet()
-
-    const allSheets = activeSpreadsheet.getSheets()
-        .map(s => s.getName());
-
-    const sheetNameEmpty = sheetName === '';
-
-    if (!sheetNameEmpty) {
-        const sheetExists = allSheets.includes(sheetName);
-
-        if (!sheetExists) {
-            return createJsonResponse({
-                status: 'error',
-                message: 'Invalid sheet name',
-            });
-        }
-
-        logSheet = activeSpreadsheet.getSheetByName(sheetName);
-    } else {
-        logSheet = activeSpreadsheet.getActiveSheet();
-    }
+    const logSheet = sheetName !== '' ?
+        activeSpreadsheet.getSheetByName(sheetName) :
+        activeSpreadsheet.getActiveSheet();
 
     if (logSheet === null) {
         return createJsonResponse({
             status: 'error',
-            message: sheetNameEmpty ? 'No spreadsheet found.' : `Spreadsheet ${sheetName} not found.`
+            message: 'No sheet found.'
         });
     }
 
-    if (fields.length < 1) {
-        setFields('name', 'email', 'message');
-    }
-
-    const length = fields.length;
+    setFields(logSheet, fields);
 
     const now = new Date();
     const date =
@@ -181,22 +127,22 @@ function action(req: ActionRequest): object {
     logSheet.insertRowAfter(1);
 
     // Filling the latest data in the second row
-    for (let idx = 0; idx < length; idx++) {
+    for (let idx = 0; idx < fields.length; idx++) {
         logSheet.getRange(2, idx + 1).setValue(jsonData[fields[idx]]);
 
-        if (idx === length - 1) {
+        if (idx === fields.length - 1) {
             logSheet.getRange(2, idx + 2).setValue(date);
         }
     }
 
-    const emailData = fields.reduce((a, c) => ({ ...a, [c]: jsonData[c] }), {});
-    const htmlBody = HtmlService.createTemplateFromFile('EmailTemplate');
-    htmlBody.data = emailData;
-    htmlBody.formHeading = formHeading;
+    if (email !== '') {
+        const emailData = fields.reduce((a, c) => ({ ...a, [c]: jsonData[c] }), {});
+        const htmlBody = HtmlService.createTemplateFromFile('EmailTemplate');
+        htmlBody.data = emailData;
+        htmlBody.formHeading = formHeading;
 
-    const emailBody = htmlBody.evaluate().getContent();
+        const emailBody = htmlBody.evaluate().getContent();
 
-    if (email) {
         MailApp.sendEmail({
             to: email,
             subject: emailSubject,
@@ -207,6 +153,6 @@ function action(req: ActionRequest): object {
 
     return createJsonResponse({
         status: 'OK',
-        message: 'Data logged successfully',
+        message: 'Submission logged successfully',
     });
 }
